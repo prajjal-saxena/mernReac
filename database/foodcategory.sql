a031885889777e3f517ab0e44485d04394f76395
unknown <prajjalsaxena@gmail.com>
2024-01-04 17:41:10 +0530

    update database

--- database/foodcategory.sql
+++ database/foodcategory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 03, 2024 at 01:39 PM
+-- Generation Time: Jan 04, 2024 at 01:04 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -40,17 +40,27 @@ CREATE TABLE `foodcategory` (
 --
 
 INSERT INTO `foodcategory` (`id`, `CategoryName`, `imgg`, `featured`, `item`) VALUES
-(1, 'Biryani', 'https://img.freepik.com/free-photo/grilled-seafood-paella-gourmet-healthy-meal-generated-by-ai_188544-39253.jpg?t=st=1703749892~exp=1703753492~hmac=c5df195498bb0e7d86a96bb1f3b853429f328b1b5de89e5b839423756af4d587&w=996', 'true', ''),
-(2, 'Starter', 'https://img.freepik.com/free-photo/crispy-french-fries-with-ketchup-mayonnaise_1150-26588.jpg?w=900&t=st=1703750753~exp=1703751353~hmac=16acd4558b6540957f31ebce7e6f1604e164b00fa35f1238a9a03fddc37b475c', 'true', ''),
-(3, 'Pizza', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'true', ''),
-(4, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', ''),
-(5, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', ''),
-(6, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', ''),
-(7, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', ''),
-(8, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', ''),
-(9, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', ''),
-(10, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', ''),
-(11, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', '');
+(1, 'Biryani', 'https://img.freepik.com/free-photo/grilled-seafood-paella-gourmet-healthy-meal-generated-by-ai_188544-39253.jpg?t=st=1703749892~exp=1703753492~hmac=c5df195498bb0e7d86a96bb1f3b853429f328b1b5de89e5b839423756af4d587&w=996', 'true', 'Biryani1'),
+(2, 'Starter', 'https://img.freepik.com/free-photo/front-view-fried-mushrooms-with-seasonings-dark-grey-table-meal-food-dinner_140725-79044.jpg?w=900&t=st=1704352150~exp=1704352750~hmac=c7c7e1ad5769745669eb1f069bfe5d45258f851fee3af883ad8ff63636909943', 'true', 'Starter1'),
+(3, 'Pizza', 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-sliced-into-six-slices_141793-2157.jpg?w=900&t=st=1704352713~exp=1704353313~hmac=391d1f433649850027cde6db29ea4b9bf1931dd8d553a8ee63c4474092993af9', 'true', 'Pizza1'),
+(4, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', 'Biryani2'),
+(5, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', 'Biryani3'),
+(6, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', 'Biryani4'),
+(7, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', 'Biryani5'),
+(8, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', 'Biryani6'),
+(9, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', 'Biryani7'),
+(10, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', 'Biryani8'),
+(11, 'Biryani', 'https://img.freepik.com/free-photo/crispy-mixed-pizza-with-olives-sausage_140725-3095.jpg?w=740&t=st=1703750889~exp=1703751489~hmac=17aa832b0ff889aa119e598b26ce093dcc28755136905c62c7049ea070bbb600', 'false', 'Biryani9'),
+(12, 'Starter', 'https://img.freepik.com/free-photo/front-view-fried-mushrooms-with-seasonings-dark-grey-table-meal-food-dinner_140725-79044.jpg?w=900&t=st=1704352150~exp=1704352750~hmac=c7c7e1ad5769745669eb1f069bfe5d45258f851fee3af883ad8ff63636909943', 'false', 'Starter2'),
+(13, 'Starter', 'https://img.freepik.com/free-photo/front-view-fried-mushrooms-with-seasonings-dark-grey-table-meal-food-dinner_140725-79044.jpg?w=900&t=st=1704352150~exp=1704352750~hmac=c7c7e1ad5769745669eb1f069bfe5d45258f851fee3af883ad8ff63636909943', 'false', 'Starter3'),
+(14, 'Starter', 'https://img.freepik.com/free-photo/front-view-fried-mushrooms-with-seasonings-dark-grey-table-meal-food-dinner_140725-79044.jpg?w=900&t=st=1704352150~exp=1704352750~hmac=c7c7e1ad5769745669eb1f069bfe5d45258f851fee3af883ad8ff63636909943', 'false', 'Starter4'),
+(15, 'Starter', 'https://img.freepik.com/free-photo/front-view-fried-mushrooms-with-seasonings-dark-grey-table-meal-food-dinner_140725-79044.jpg?w=900&t=st=1704352150~exp=1704352750~hmac=c7c7e1ad5769745669eb1f069bfe5d45258f851fee3af883ad8ff63636909943', 'false', 'Starter5'),
+(16, 'Starter', 'https://img.freepik.com/free-photo/front-view-fried-mushrooms-with-seasonings-dark-grey-table-meal-food-dinner_140725-79044.jpg?w=900&t=st=1704352150~exp=1704352750~hmac=c7c7e1ad5769745669eb1f069bfe5d45258f851fee3af883ad8ff63636909943', 'false', 'Starter6'),
+(17, 'Pizza', 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-sliced-into-six-slices_141793-2157.jpg?w=900&t=st=1704352713~exp=1704353313~hmac=391d1f433649850027cde6db29ea4b9bf1931dd8d553a8ee63c4474092993af9', 'false', 'Pizza2'),
+(18, 'Pizza', 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-sliced-into-six-slices_141793-2157.jpg?w=900&t=st=1704352713~exp=1704353313~hmac=391d1f433649850027cde6db29ea4b9bf1931dd8d553a8ee63c4474092993af9', 'false', 'Pizza3'),
+(19, 'Pizza', 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-sliced-into-six-slices_141793-2157.jpg?w=900&t=st=1704352713~exp=1704353313~hmac=391d1f433649850027cde6db29ea4b9bf1931dd8d553a8ee63c4474092993af9', 'false', 'Pizza4'),
+(20, 'Pizza', 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-sliced-into-six-slices_141793-2157.jpg?w=900&t=st=1704352713~exp=1704353313~hmac=391d1f433649850027cde6db29ea4b9bf1931dd8d553a8ee63c4474092993af9', 'false', 'Pizza5'),
+(21, 'Pizza', 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-sliced-into-six-slices_141793-2157.jpg?w=900&t=st=1704352713~exp=1704353313~hmac=391d1f433649850027cde6db29ea4b9bf1931dd8d553a8ee63c4474092993af9', 'false', 'Pizza6');
 
 --
 -- Indexes for dumped tables
@@ -70,7 +80,7 @@ ALTER TABLE `foodcategory`
 -- AUTO_INCREMENT for table `foodcategory`
 --
 ALTER TABLE `foodcategory`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
